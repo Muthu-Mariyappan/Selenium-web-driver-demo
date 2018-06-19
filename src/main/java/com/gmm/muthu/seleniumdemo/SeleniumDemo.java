931a70e2e520e2a45927cae453c2eac431d0e6b9
Muthu Mariyappan <ggmm.muthu@gmail.com>
2018-06-19 07:40:38 +0530

    Update SeleniumDemo.java

--- src/main/java/com/gmm/muthu/seleniumdemo/SeleniumDemo.java
+++ src/main/java/com/gmm/muthu/seleniumdemo/SeleniumDemo.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * It is a simple demo of selenium.WebDriver with different implementations to get specific info from target web page
  * I used ChromeDriver and FirefoxDriver for demonstration, similarly we can use different implementation of WebDriver
- * For demonstration of different implementation of WebDriver, I used ChoremDriver to print info about Messi and FirefoxDriver for ronaldo
+ * For demonstration of different implementation of WebDriver, I used ChromeDriver to print info about Messi and FirefoxDriver for ronaldo
  * */
 public class SeleniumDemo {
 	
